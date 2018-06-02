3c1e543630971a936fc624dcce5d61a9baff1faf
ChamodNimz <34089218+ChamodNimz@users.noreply.github.com>
2018-06-02 02:05:17 -0700

    Add files via upload
    connection class need to create ... this is the main basic php templatet edition starting deep coding from now on

---
+++ navigation.php
@@ -0,0 +1,99 @@
+<!--Header | Start-->
+	<div class="header current-menu-item center" style="animation-duration: 2s; animation-name: exampless;" >
+		<div class="logo">
+            <a href="index.php" ><img src="Hotel/logo.png"></a>
+		</div>
+		<!-- Navigation | START -->
+		<nav>
+            <ul>
+                <li class="current-menu-item">
+					<a href="index.php"><span style="color:#56D5C4; !important">Home</span></a>
+                    	<ul style="top: 95px;">
+                        	<li class="current-menu-item"><a href="Jasper hotel Home .html">Single Hotel</a></li>
+                            <li><a href="Multiple Hotels.html">Multiple Hotels</a></li>
+                        </ul>
+                </li>
+               <li><a href="accomodation.php"><span>Accommodation</span></a>
+					<ul style="top: 95px;">
+                        <li class="has-children"><a href="#">Rooms</a>
+                        	 <ul style="top: 95px;">
+                                <li><a href="Deluxe Room.html">Deluxe Room</a></li>
+                                <li><a href="#">Executive Room</a></li>
+                                <li><a href="#">Premium Room</a></li>
+                                <li><a href="#">Superior Room</a></li>
+                            </ul>
+                        </li>
+                        <li class="has-children"><a href="#">Suites</a>
+                        	 <ul style="top: 95px;">
+                                <li><a href="#">Suite</a></li>
+                                <li><a href="#">The Jasper Suite</a></li>
+                            </ul>
+                        </li>
+                        <li class="has-children"><a href="#">Residences</a>
+                        	 <ul style="top: 95px;">
+                                <li><a href="#">3Bedroom - Ocean  View</a></li>
+                                <li><a href="#">3Bedroom - Deluxe City View</a></li>
+                                <li><a href="#">3Bedroom - Premium City View</a></li>
+                                <li><a href="#">3Bedroom - Superior View</a></li>
+                                <li><a href="#">4Bedroom - Ocean Premier</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li><a href="aboutUs.php"><span>About Hotel</span></a>
+					<ul style="top: 95px;">
+						<li><a href="gallery.php">Photo Gallery</a></li>
+                        <li class="has-children"><a href="#">Special Offers</a>
+                            <ul style="top: 95px;">
+                                <li><a href="#">Specials - List</a></li>
+                                <li><a href="#">Specials - Single</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Location</a></li>
+                        <li><a href="#">Restaurant</a></li>
+                        <li class="has-children"><a href="#">Blog</a>
+                            <ul style="top: 95px;">
+                                <li><a href="#">Blog - List</a></li>
+                                <li><a href="#">Blog - Single</a></li>
+                            </ul>
+                        </li>
+						<li><a href="#">Guest Book</a></li>
+                        <li><a href="#">FAQss</a></li>
+                    </ul>   
+                </li>
+                <li><a href="contactUs.php">Contact Us</a></li>
+				<!-- Search | START -->
+                        <li class="menu-parent icon mega-menu">
+                            <a><span style="top:40px; position:absolute;"><i class="icon  fa fa-search"></i></span><div>Search</div></a>
+                            <div class="drop search-drop" style="display: none;">
+                                <div class="center">
+                                    <div class="drop-container">
+                                        <form id="search" action="#" method="get">
+                                            <input name="s" class="search-field" type="text" placeholder="What are you looking for?" autocomplete="off" style="width: 232.25px;">
+                                            <button type="submit" class="button"><span>See Results</span></button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                <!-- Search | END -->          
+			</ul>
+		</nav>
+        <!-- Navigation | END -->
+        <div class="book-nav">
+                    <a href="booking.php" class="button">Book <i class="icon fa fa-check-circle"></i></a>
+                <!-- Language | START -->
+                    <div id="language">
+                        <ul>
+                            <li class="en"><a>EN</a></li>
+                            <li class="cn"><a href="#">CN</a></li>
+                            <li class="fr"><a href="#">FR</a></li>
+                            <li class="de"><a href="#">DE</a></li>
+                            <li class="it"><a href="#">IT</a></li>
+                        </ul>
+                    </div>
+                <!-- Language | END -->
+		</div>	<div class="nav-cover"></div>
+    <div class="nav-header"></div>	
+	</div>  
+    <!--Header  |   END-->  
